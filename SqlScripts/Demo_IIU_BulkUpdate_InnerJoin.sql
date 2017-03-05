fe6129c8e05164ba3c7b7a003fb829d13490e639
ceeae <ceeaeprj@gmail.com>
2017-03-05 14:49:28 +0100

    Changed package name - added comments to sql scripts

--- SqlScripts/Demo_IIU_BulkUpdate_InnerJoin.sql
+++ SqlScripts/Demo_IIU_BulkUpdate_InnerJoin.sql
@@ -1,3 +1,6 @@
+-- Bulk update existing rows in VOutput with corresponding "fresh" data in VInput
+-- Key (NumeroAtto AND DataDocumento)
+
 UPDATE VOutput
 SET VOutput.LastModifiedOn = GETUTCDATE(), VOutput.NumeroAtto = A.NumeroAtto, VOutput.DataDocumento = A.DataDocumento, VOutput.Test = A.Test
 FROM VInput A
--- SqlScripts/Demo_IIU_SetupTables.sql
+++ SqlScripts/Demo_IIU_SetupTables.sql
@@ -1,4 +1,10 @@
--- Create a 'View' kind of table
+
+-- Disclaimer 
+-- This is a demo that shows how to implement incremental data upload 
+-- from an input table or view (VInput) to an output table.
+-- The script creates two table VInput and VOutput with some demostration data.
+
+-- Create VInput table
 
 IF EXISTS (SELECT 1 FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = 'VInput')
 	DROP TABLE [dbo].[VInput]
@@ -8,6 +14,8 @@ CREATE TABLE [dbo].[VInput](
 	[DataDocumento] [date] NOT NULL,
 	[Test] [varchar](50) NULL
 ) ON [PRIMARY]
+
+-- Create VOutput table
 
 IF EXISTS (SELECT 1 FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = 'VOutput')
 	DROP TABLE [dbo].[VOutput]
@@ -27,34 +35,14 @@ ALTER TABLE [dbo].[VOutput]
 
 GO
 
--- Insert some demo data [VInput]
+-- Insert some demo data to VInput
+
 INSERT INTO [dbo].[VInput] ([NumeroAtto],[DataDocumento],[Test]) VALUES ('0000000001', '2017-01-01', 'Test 1')
 INSERT INTO [dbo].[VInput] ([NumeroAtto],[DataDocumento],[Test]) VALUES ('0000000002', '2017-01-01', 'Test 2')
 INSERT INTO [dbo].[VInput] ([NumeroAtto],[DataDocumento],[Test]) VALUES ('0000000003', '2017-01-02', 'Test 3')
 
--- Insert some demo data [VOutput]
+-- Insert some demo data to VOutput
+
 INSERT INTO [dbo].[VOutput] ([NumeroAtto],[DataDocumento],[Test]) VALUES ('0000000001', '2017-01-01', 'Test 4')
---INSERT INTO [dbo].[VOutput] ([NumeroAtto],[DataDocumento],[Test]) VALUES ('0000000002', '2017-01-01', 'Test 2')
---INSERT INTO [dbo].[VOutput] ([NumeroAtto],[DataDocumento],[Test]) VALUES ('0000000003', '2017-01-02', 'Test 3')
 
 GO
-
--- BULK INSERT
--- ===========
-
---INSERT INTO VOutput (NumeroAtto, DataDocumento, Test)
-
---	SELECT A.NumeroAtto, A.DataDocumento, A.Test
---	FROM VInput A
---	LEFT JOIN VOutput B
---	ON A.NumeroAtto = B.NumeroAtto AND A.DataDocumento = B.DataDocumento
---	WHERE B.DataDocumento IS NULL AND B.NumeroAtto IS NULL
-
--- BULK UPDATE
--- ===========
-
---UPDATE VOutput 
---SET VOutput.LastModifiedOn = GETUTCDATE(), VOutput.NumeroAtto = A.NumeroAtto, VOutput.DataDocumento = A.DataDocumento, VOutput.Test = A.Test
---FROM VInput A
---	INNER JOIN VOutput B
---	ON A.NumeroAtto = B.NumeroAtto AND A.DataDocumento = B.DataDocumento
